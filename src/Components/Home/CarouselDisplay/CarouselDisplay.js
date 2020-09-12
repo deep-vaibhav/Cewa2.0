1c0c9089c9f4939914f3583493a98e9fb14e5a69
Vaibhav <vaibhavdeep46@gmail.com>
2020-09-12 16:32:00 +0530

    explore link

--- src/Components/Home/CarouselDisplay/CarouselDisplay.js
+++ src/Components/Home/CarouselDisplay/CarouselDisplay.js
@@ -55,7 +55,7 @@ class CarouselDisplay extends React.Component {
                             Every child deserves a chance at life, the right to follow their dreams, an opportunity to embrace their potential, and some humanity, and care. Therefore, every child must be treated fair. If we stand up for the impoverished children, we stand for a better society, a better tomorrow, and a brighter future.
                         </motion.p>
                         <motion.a 
-                            href="/events"
+                            href="/Events"
                             whileHover={{ scale: 1.05,
                                             y: -4,
                                          }}
